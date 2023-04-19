64e1821eb981e241af1d62473d828203e36cb161
Gustavo <gustavo.prog40@gmail.com>
2023-04-19 18:37:38 -0300

    login

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import {Request, Response} from 'express'
 const app = require('express')()
 const server = require('http').createServer(app)
 // const io = require('socket.io')(server, { cors: { origin: 'http://localhost:5173' } })
-const io = require('socket.io')(server, { cors: { origin: 'https://web-chat-my.netlify.app/' } })
+// const io = require('socket.io')(server, { cors: { origin: 'https://web-chat-my.netlify.app/' } })
+const io = require('socket.io')(server, { cors: { origin: '*' } })
 
 
 
